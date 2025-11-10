1430bdbb154e9449bbb85f004cd0b8b337e75876
codebyalok1 <alok.gupta_cs24@gla.ac.in>
2025-11-10 11:25:36 +0530

    Add New Folder

---
+++ Programming_Elements/Level2/Exception.java
@@ -0,0 +1,15 @@
+package com.Exceptiondemo;
+
+public class Exception {
+    public static void main(String[] args) {
+        int a =10;
+        int b =0;
+        try{
+            int result=a/b;
+            System.out.println(result);
+        }
+        catch( ArithmeticException e){
+            System.out.println("Found");
+        }
+    }
+}
